75662286a971080a4ec691f2189a41b4cc134fef
hijaxxed <hetheringtond.dh@gmail.com>
2018-03-02 21:39:44 +0000

    Added more pages

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+title = 'Dev Bliss';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dev Bliss' });
+  res.render('index', {title});
 });
 
 router.get('/ph', function(req, res, next) {
-  res.render('ph');
+  res.render('ph', { title});
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {title});
+});
+
+
+router.get('/ex', function(req, res, next) {
+  res.render('devex', {title});
+});
+
 module.exports = router;
