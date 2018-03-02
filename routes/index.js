29323484963af76c1aa97edac11a24ec91c7319e
hijaxxed <hetheringtond.dh@gmail.com>
2018-03-02 19:46:08 +0000

    Added sticky header and an image banner

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dev Bliss' });
 });
 
+router.get('/ph', function(req, res, next) {
+  res.render('ph');
+});
 module.exports = router;
