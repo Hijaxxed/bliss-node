ca2e8cf0fda82baa6ec1ed41431f3bd2fd986d9b
hijaxxed <hetheringtond.dh@gmail.com>
2018-03-07 22:14:02 +0000

    Removed header background image and tweaked content margins

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-title = 'Dev Bliss';
+title = 'Lethargic Bliss';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
